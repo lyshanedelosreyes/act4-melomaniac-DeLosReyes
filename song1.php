13275820ab80b477bf5832ab95fb2f648295396c
DeLosReyesLyshane <lyshanedelosreyes@gmail.com>
2022-05-24 15:40:54 +0800

    Song 1 added

---
+++ song1.php
@@ -0,0 +1,153 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> iLyrics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Line Without A Hook</h2>";
+                        echo "<p class='lead '>Ricky Montgomery</p>";
+
+                        echo "<p><pre>                                        G                    Em</pre></p>";
+                        echo "<p> I don't really give a damn about the way you touch me when we're alone</p>";
+                        echo "<p><pre>C               D</pre></p>";
+                        echo "<p> You can hold my hand if no one's home</p>";
+                        echo "<p><pre>    G               Em</pre></p>";
+                        echo "<p> Do you like it when I'm away?</p>";
+                        echo "<p><pre>                  C                                 D</pre></p>";
+                        echo "<p> If I went and hurt my body, baby, would you love me the same?</p>";
+                        echo "<p><pre>              G                         Em</pre></p>";
+                        echo "<p> I can feel all my bones coming back and I'm craving motion</p>";
+                        echo "<p><pre>C                           D</pre></p>";
+                        echo "<p> Mama never really learned how to live by herself</p>";
+                        echo "<p><pre>    G              Em                C               D</pre></p>";
+                        echo "<p> It's a curse and it's growing; you're a pond and I'm an ocean</p>";
+                        echo "<p><pre>G       Em      C           D                      G</pre></p>";
+                        echo "<p> All my emotions feel like explosions when you are around</p>";
+                        echo "<p><pre>   Em        C               D</pre></p>";
+                        echo "<p> And I've found a way to kill the sound</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>G                Em                 C</pre></p>";
+                        echo "<p> Oh, baby, I am a wreck when I'm without you</p>";
+                        echo "<p><pre>  D                G</pre></p>";
+                        echo "<p> I need you here to stay</p>";
+                        echo "<p><pre>               Em               C</pre></p>";
+                        echo "<p> I broke all my bones that day I found you </p>";
+                        echo "<p><pre>D             G</pre></p>";
+                        echo "<p> Crying at the lake</p>";
+                        echo "<p><pre>                   Em               C                  D</pre></p>";
+                        echo "<p> Was it something I said to make you feel like you're a burden?</p>";
+                        echo "<p><pre>      G                Em              C       D               G  Em  C</pre></p>";
+                        echo "<p> Oh, and if I could take it all back I swear that I would pull you from the tide</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>D                        G</pre></p>";
+                        echo "<p> Oh woah woah woah I said no</p>";
+                        echo "<p><pre>       Em</pre></p>";
+                        echo "<p> I said no</p>";
+                        echo "<p><pre>       C             D</pre></p>";
+                        echo "<p> Listen close: it's a no</p>";
+                        echo "<p><pre>    G                  Em</pre></p>";
+                        echo "<p> The wind is a'poundin' on my back </p>";
+                        echo "<p><pre>         C               D</pre></p>";
+                        echo "<p> And I've found hope in a heart attack </p>";
+                        echo "<p><pre>       G          Em</pre></p>";
+                        echo "<p> Oh, at last it is passed</p>";
+                        echo "<p><pre>         C                    D</pre></p>";
+                        echo "<p> Now I've got it and you can't have it</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>G                Em                 C</pre></p>";
+                        echo "<p> Oh, baby, I am a wreck when I'm without you</p>";
+                        echo "<p><pre>  D                G</pre></p>";
+                        echo "<p> I need you here to stay</p>";
+                        echo "<p><pre>               Em               C</pre></p>";
+                        echo "<p> I broke all my bones that day I found you </p>";
+                        echo "<p><pre>D             G</pre></p>";
+                        echo "<p> Crying at the lake</p>";
+                        echo "<p><pre>                   Em               C                  D</pre></p>";
+                        echo "<p> Was it something I said to make you feel like you're a burden?</p>";
+                        echo "<p><pre>        G                      Em                C       D</pre></p>";
+                        echo "<p> Oh, and if I could take it all back I swear that I would pull you from the</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>Am   G  Bm   Am          G        Bm</pre></p>";
+                        echo "<p> Tiiiiiiiide, oh oh oh oh oh oh oh oh oh.  </p>";
+                        echo "<p><pre>Am               G     Bm         Am               G        Bm</pre></p>";
+                        echo "<p> Darlin, when I'm fast asleep I've seen this person watching me sayin,</p>";
+                        echo "<p><pre>Am     G    Bm      Am        D      G  Em  C  D</pre></p>";
+                        echo "<p>  Is it worth it?  Is it worth it? Tell me is it worth it?</p>";
+                        echo "<p><pre> G Em C D                G                      Em</pre></p>";
+                        echo "<p> Ohhhhhhh, cause there is something and there is nothing</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>         C            D</pre></p>";
+                        echo "<p> There is nothing in between</p>";
+                        echo "<p><pre>           G                Em          C              D</pre></p>";
+                        echo "<p> And, in my eyes, there is a tiny dancer  watching over me</p>";
+                        echo "<p><pre>              G                Em        C           D</pre></p>";
+                        echo "<p> He's singin' 'she's a, she's a lady' and I am just a boy</p>";
+                        echo "<p><pre>              G                Em        C           D</pre></p>";
+                        echo "<p> He's singing 'she's a, she's a lady' and I am just a line without a (hook)</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>G                Em                 C</pre></p>";
+                        echo "<p> Oh, baby, I am a wreck when I'm without you</p>";
+                        echo "<p><pre>  D                G</pre></p>";
+                        echo "<p> I need you here to stay</p>";
+                        echo "<p><pre>               Em               C</pre></p>";
+                        echo "<p> I broke all my bones that day I found you </p>";
+                        echo "<p><pre>D             G</pre></p>";
+                        echo "<p> Crying at the lake</p>";
+                        echo "<p><pre>                   Em               C                  D</pre></p>";
+                        echo "<p> Was it something I said to make you feel like you're a burden?</p>";
+                        echo "<p><pre>        G                      Em                C       D                 G</pre></p>";
+                        echo "<p> Oh, and if I could take it all back I swear that I would pull you from the tide</p>";
+                        echo "<br/>";
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Lyshane De Los Reyes</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
