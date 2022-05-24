eddaf399b0d673e16ff716fe760ba4cf4e8a08d0
DeLosReyesLyshane <lyshanedelosreyes@gmail.com>
2022-05-24 17:08:31 +0800

    Song 4 added

---
+++ song4.php
@@ -0,0 +1,151 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> iLyrics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>All We Know</h2>";
+                        echo "<p class='lead '>The Chainsmokers (feat. Phoebe Ryan)</p>";
+
+                        echo "<p><pre>C                F</pre></p>";
+                        echo "<p> Fighting flames of fire</p>";
+                        echo "<p><pre>Am</pre></p>";
+                        echo "<p> Hang onto burning wires</p>";
+                        echo "<p><pre>C              F  Am</pre></p>";
+                        echo "<p> We don’t care anymore</p>";
+                        echo "<p><pre>C        F</pre></p>";
+                        echo "<p> Are we fading lovers?</p>";
+                        echo "<p><pre>Am</pre></p>";
+                        echo "<p> We keep wasting colors</p>";
+                        echo "<p><pre>C       F                Am</pre></p>";
+                        echo "<p> Maybe we should let this go</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>      Am                      F            C</pre></p>";
+                        echo "<p> We’re falling apart, still we hold together</p>";
+                        echo "<p><pre>      Am                    F            C</pre></p>";
+                        echo "<p> We’ve passed the end, so we chase forever</p>";
+                        echo "<p><pre>       Am             F    C</pre></p>";
+                        echo "<p> ’Cause this is all we know</p>";
+                        echo "<p><pre>     Am               F    C</pre></p>";
+                        echo "<p> This feeling’s all we know</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>     F                      G</pre></p>";
+                        echo "<p> I’ll ride my bike up to the world</p>";
+                        echo "<p><pre>         Am</pre></p>";
+                        echo "<p> Down the streets right through the city</p>";
+                        echo "<p><pre>        F              G</pre></p>";
+                        echo "<p> I’ll go everywhere you go</p>";
+                        echo "<p><pre>     Am</pre></p>";
+                        echo "<p> From Chicago to the coast</p>";
+                        echo "<p><pre>              F                  G</pre></p>";
+                        echo "<p> You tell me, “Hit this and let’s go</p>";
+                        echo "<p><pre>         Am</pre></p>";
+                        echo "<p> Blow the smoke right through the window</p>";
+                        echo "<p><pre>       F              G     Am</pre></p>";
+                        echo "<p> ’Cause this is all we know</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>Am F C</pre></p>";
+                        echo "<p><pre>Am F       C</pre></p>";
+                        echo "<p> ’Cause this is all we know</p>";
+                        echo "<p><pre>Am F C</pre></p>";
+                        echo "<p><pre>Am F       C</pre></p>";
+                        echo "<p> ’Cause this is all we know</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>C        F</pre></p>";
+                        echo "<p> Never face each other</p>";
+                        echo "<p><pre>Am</pre></p>";
+                        echo "<p> One but different covers</p>";
+                        echo "<p><pre>C           F       Am</pre></p>";
+                        echo "<p> We don’t care anymore</p>";
+                        echo "<p><pre>C            F</pre></p>";
+                        echo "<p> Two hearts still beating</p>";
+                        echo "<p><pre>Am</pre></p>";
+                        echo "<p> On with different rhythms</p>";
+                        echo "<p><pre>C           F       Am</pre></p>";
+                        echo "<p> Maybe we should let this go</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>      Am                      F            C</pre></p>";
+                        echo "<p> We’re falling apart, still we hold together</p>";
+                        echo "<p><pre>      Am                    F            C</pre></p>";
+                        echo "<p> We’ve passed the end, so we chase forever</p>";
+                        echo "<p><pre>       Am             F    C</pre></p>";
+                        echo "<p> ’Cause this is all we know</p>";
+                        echo "<p><pre>     Am               F    C</pre></p>";
+                        echo "<p> This feeling’s all we know</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>     F                      G</pre></p>";
+                        echo "<p> I’ll ride my bike up to the world</p>";
+                        echo "<p><pre>         Am</pre></p>";
+                        echo "<p> Down the streets right through the city</p>";
+                        echo "<p><pre>        F              G</pre></p>";
+                        echo "<p> I’ll go everywhere you go</p>";
+                        echo "<p><pre>     Am</pre></p>";
+                        echo "<p> From Chicago to the coast</p>";
+                        echo "<p><pre>              F                  G</pre></p>";
+                        echo "<p>               F                  G</p>";
+                        echo "<p><pre>              F                  G</pre></p>";
+                        echo "<p> Blow the smoke right through the window</p>";
+                        echo "<p><pre>       F              G     Am</pre></p>";
+                        echo "<p> ’Cause this is all we know</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>Am F C</pre></p>";
+                        echo "<p><pre>Am F       C</pre></p>";
+                        echo "<p> ’Cause this is all we know</p>";
+                        echo "<p><pre>Am F C</pre></p>";
+                        echo "<p><pre>Am F       C</pre></p>";
+                        echo "<p> ’Cause this is all we know</p>";
+                        echo "<br/>";
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Lyshane De Los Reyes</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
