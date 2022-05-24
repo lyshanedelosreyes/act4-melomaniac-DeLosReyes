32a28fa0b2b6783e9c3d06387cd4af54f2e820a1
DeLosReyesLyshane <lyshanedelosreyes@gmail.com>
2022-05-24 17:27:25 +0800

    Song 5 added

---
+++ song5.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Melomaniac Activity</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> iLyrics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+                        
+                        <?php
+
+                        echo "<h2 class='text-primary'>Lover</h2>";
+                        echo "<p class='lead '>Taylor Swift</p>";
+
+                        echo "<p><pre>G                  D                        C</pre></p>";
+                        echo "<p> We could leave the Christmas lights up 'til January</p>";
+                        echo "<p><pre>G           D                   C</pre></p>";
+                        echo "<p> This is our place, we make the rules</p>";
+                        echo "<p><pre>G                              D               C</pre></p>";
+                        echo "<p> And there's a dazzling haze, a mysterious way about you, dear</p>";
+                        echo "<p><pre>G                   D              C</pre></p>";
+                        echo "<p> Have I known you 20 seconds or 20 years?</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>    G     D         C</pre></p>";
+                        echo "<p> Can I go where you go?</p>";
+                        echo "<p><pre>       G      D       C</pre></p>";
+                        echo "<p> Can we always be this close forever and ever?</p>";
+                        echo "<p><pre>    G            D               C</pre></p>";
+                        echo "<p> And ah, take me out, and take me home</p>";
+                        echo "<p><pre>       G   D/F# Em  D  C N.C.</pre></p>";
+                        echo "<p> You're my, my, my, my lover</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>G                 D                    C</pre></p>";
+                        echo "<p> We could let our friends crash in the living room</p>";
+                        echo "<p><pre> G           D                 C</pre></p>";
+                        echo "<p> This is our place, we make the call</p>";
+                        echo "<p><pre>        G                      D                     C</pre></p>";
+                        echo "<p> And I'm highly suspicious that everyone who sees you wants you</p>";
+                        echo "<p><pre> G                                D                     C</pre></p>";
+                        echo "<p> I've loved you three summers now, honey, but I want 'em all</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>    G     D         C</pre></p>";
+                        echo "<p> Can I go where you go?</p>";
+                        echo "<p><pre>       G      D       C</pre></p>";
+                        echo "<p> Can we always be this close forever and ever?</p>";
+                        echo "<p><pre>    G            D               C</pre></p>";
+                        echo "<p> And ah, take me out, and take me home</p>";
+                        echo "<p><pre>       G   D/F# Em  D  C N.C.</pre></p>";
+                        echo "<p> You're my, my, my, my lover</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>G                     D/F#</pre></p>";
+                        echo "<p> Ladies and gentlemen, will you please stand?</p>";
+                        echo "<p><pre>Em                       G7/F</pre></p>";
+                        echo "<p> With every guitar string scar on my hand</p>";
+                        echo "<p><pre>C                    Em/B                 Am     D</pre></p>";
+                        echo "<p> I take this magnetic force of a man to be my ... lover</p>";
+                        echo "<p><pre>G                            D/F#</pre></p>";
+                        echo "<p> My heart's been borrowed and yours has been blue</p>";
+                        echo "<p><pre>Em                           G7/F</pre></p>";
+                        echo "<p> All's well that ends well to end up with you</p>";
+                        echo "<p><pre>C                  Em/B              Am     D</pre></p>";
+                        echo "<p> Swear to be overdramatic and true to my ... lover</p>";
+                        echo "<p><pre>    G                    G7/F             C</pre></p>";
+                        echo "<p> And you'll save all your dirtiest jokes for me</p>";
+                        echo "<p><pre>             Am                     D</pre></p>";
+                        echo "<p> And at every table, I'll save you a seat, lover</p>";
+                        echo "<br/>";
+
+                        echo "<p><pre>    G     D         C</pre></p>";
+                        echo "<p> Can I go where you go?</p>";
+                        echo "<p><pre>       G      D       C</pre></p>";
+                        echo "<p> Can we always be this close forever and ever?</p>";
+                        echo "<p><pre>    G            D               C</pre></p>";
+                        echo "<p> And ah, take me out, and take me home (Forever and ever)</p>";
+                        echo "<p><pre>       G   D/F# Em  D  C</pre></p>";
+                        echo "<p> You're my, my,  my, my</p>";
+                        echo "<p><pre>           G   D/F# Em  D  C</pre></p>";
+                        echo "<p> Oh, you're my, my,  my, my</p>";
+                        echo "<p><pre>                G   D/F# Em  D  C N.C.</pre></p>";
+                        echo "<p> Darling, you're my, my,  my, my   lover</p>";
+                        echo "<br/>";
+                        ?>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Lyshane De Los Reyes</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
